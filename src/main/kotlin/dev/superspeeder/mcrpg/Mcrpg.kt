795920996bdc1cf6cd668c997c3def90fbcf7759
Andy Newton <andy@delusion.org>
2024-12-09 18:19:08 -0800

    initial commit

---
+++ src/main/kotlin/dev/superspeeder/mcrpg/Mcrpg.kt
@@ -0,0 +1,54 @@
+package dev.superspeeder.mcrpg
+
+import net.minecraft.client.Minecraft
+import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.fml.common.Mod
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent
+import net.neoforged.fml.event.lifecycle.FMLDedicatedServerSetupEvent
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
+import thedarkcolour.kotlinforforge.neoforge.forge.runForDist
+
+/**
+ * Main mod class.
+ *
+ * An example for blocks is in the `blocks` package of this mod.
+ */
+@Mod(Mcrpg.ID)
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
+object Mcrpg {
+    const val ID = "mcrpg"
+
+    val LOGGER: Logger = LogManager.getLogger(ID)
+
+    init {
+        LOGGER.log(Level.INFO, "Hello world!")
+
+        val obj = runForDist(clientTarget = {
+            MOD_BUS.addListener(::onClientSetup)
+            Minecraft.getInstance()
+        }, serverTarget = {
+            MOD_BUS.addListener(::onServerSetup)
+            "test"
+        })
+
+        println(obj)
+    }
+
+    private fun onClientSetup(event: FMLClientSetupEvent) {
+        LOGGER.log(Level.INFO, "Initializing client...")
+    }
+
+    private fun onServerSetup(event: FMLDedicatedServerSetupEvent) {
+        LOGGER.log(Level.INFO, "Server starting...")
+    }
+
+    @SubscribeEvent
+    fun onCommonSetup(event: FMLCommonSetupEvent) {
+        LOGGER.log(Level.INFO, "Hello! This is working!")
+    }
+}
